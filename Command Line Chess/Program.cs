ea42964cb20fd9881196bb3ac982e70d2d9e225f
Chris <applepie1928@gmail.com>
2021-01-05 00:19:26 +0000

    Add project files.

---
+++ Command Line Chess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Command_Line_Chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
